56e21f51d3dbf3b423cb162a33f819ca81e7ce5e
yh <>
2010-12-15 14:56:00 -0800

    def getInputFileList(self, file_source_ls, tmp_dir): 		""" 		2010-12-14 			create new directory if tmp_dir is not present
    	def run(self):
    		"""
    		2010-12-15
    			add URLs for human, chimp, macaque genome sequences.

--- transfac/src/UpdateGenomeDB.py
+++ transfac/src/UpdateGenomeDB.py
@@ -4,12 +4,21 @@ Examples:
 	#postgres
 	UpdateGenomeDB.py -g at -c
 	
+	# 2010-12-14 add human genome sequences into vervetdb.genome.
+	UpdateGenomeDB.py -g hs -d vervetdb -k genome -u yh -c -v postgresql -t /usr/local/research_data/NCBI/hs
+	
+	# 2010-12-14 add chimp genome sequences into vervetdb.genome.
+	UpdateGenomeDB.py -g ptr -d vervetdb -k genome -u yh -c -v postgresql -t /usr/local/research_data/NCBI/ptr
+	
 	#mysql
 	UpdateGenomeDB.py -v mysql -u yh -z papaya -d genome -k "" -g at -c
 	
 Description:
 	A wrapper program to fill database with genome sequences. Call chromosome_fasta2db for each file downloaded.
-
+	
+	The sequence file from NCBI contains the scientific name of the species,
+		which would be used to find out the tax-id through querying tables in the taxonomy schema.
+	The organism argument of the program is only used to download sequences from web.
 """
 import sys, os, math
 bit_number = math.log(sys.maxint)/math.log(2)
@@ -32,8 +41,8 @@ class UpdateGenomeDB(object):
 							('schema', 1, ): ['genome', 'k', 1, 'database schema name', ],\
 							('db_user', 1, ): [None, 'u', 1, 'database username', ],\
 							('db_passwd', 1, ): [None, 'p', 1, 'database password', ],\
-							('organism', 0, ): [None, 'g', 1, '2-letter abbreviation for organism. Optional, if specified, only sequence from this organism would be extracted.', ],\
-							('tmp_dir',1,):['/tmp/', 't', 1, 'Temporary directory to hold files downloaded from web'],\
+							('organism', 0, ): [None, 'g', 1, '2-letter abbreviation for organism. used to download sequence files.', ],\
+							('tmp_dir',1,):['/tmp/', 't', 1, 'Temporary directory to hold files from web. Will be created if not present'],\
 							('commit', 0, int):[0, 'c', 0, 'commit db transaction'],\
 							('debug', 0, int):[0, 'b', 0, 'toggle debug mode'],\
 							('report', 0, int):[0, 'r', 0, 'toggle report, more verbose stdout/stderr.']}
@@ -52,6 +61,8 @@ class UpdateGenomeDB(object):
 		
 	def getInputFileList(self, file_source_ls, tmp_dir):
 		"""
+		2010-12-14
+			create new directory if tmp_dir is not present
 		2008-07-06
 		"""
 		sys.stderr.write("Getting input file list ...")
@@ -61,8 +72,10 @@ class UpdateGenomeDB(object):
 			filename = os.path.basename(file_source)
 			local_filename = os.path.join(tmp_dir, filename)
 			if os.path.isfile(local_filename):
-				sys.stderr("%s exists. Skip.\n"%local_filename)
+				sys.stderr.write("%s exists. Skip.\n"%local_filename)
 			else:
+				if not os.path.isdir(tmp_dir):
+					os.makedirs(tmp_dir)
 				cmd_p = subprocess.Popen([command, '-P', tmp_dir, file_source], stderr=sys.stderr, stdout=sys.stdout)
 				cmd_p.wait()
 			file_list.append(local_filename)
@@ -70,12 +83,44 @@ class UpdateGenomeDB(object):
 		return file_list
 	
 	def run(self):
+		"""
+		2010-12-15
+			add URLs for human, chimp, macaque genome sequences.
+		"""
+		#2010-12-14 URl for the human genome sequences
+		speciesID = 'hs'
+		self.short_org2chr_file_ls[speciesID] = []
+		for chr in range(1,23)+['X', 'Y','MT']:
+			self.short_org2chr_file_ls[speciesID].\
+				append('ftp://ftp.ncbi.nih.gov/refseq/H_sapiens/H_sapiens/Assembled_chromosomes/seq/hs_ref_GRCh37.p2_chr%s.fa.gz'%chr)
+		
+		speciesID = 'mm'
+		#2010-12-14 URl for the rhesus macaque genome sequences
+		self.short_org2chr_file_ls[speciesID] = []
+		for chr in range(1,21)+['X',]:
+			self.short_org2chr_file_ls[speciesID].\
+				append('ftp://ftp.ncbi.nlm.nih.gov/genomes/Macaca_mulatta/Assembled_chromosomes/seq/mmu_ref_Mmul_051212_chr%s.fa.gz'%chr)
+		self.short_org2chr_file_ls[speciesID].\
+				append('ftp://ftp.ncbi.nlm.nih.gov/genomes/Macaca_mulatta/Assembled_chromosomes/seq/mmu_chrMT.fa.gz')
+		
+		speciesID = 'ptr'
+		#2010-12-14 URl for the rhesus macaque genome sequences
+		self.short_org2chr_file_ls[speciesID] = []
+		for chr in range(1,23)+['X','Y', '2A', '2B']:
+			self.short_org2chr_file_ls[speciesID].\
+				append('ftp://ftp.ncbi.nlm.nih.gov/genomes/Pan_troglodytes/Assembled_chromosomes/ptr_ref_chr%s.fa.gz'%chr)
+		
 		file_list = self.getInputFileList(self.short_org2chr_file_ls[self.organism], self.tmp_dir)
 		for filename in file_list:
-			cf2db_ins = chromosome_fasta2db(drivername=self.drivername, hostname=self.hostname, dbname=self.dbname, schema=self.schema, \
-										db_user=self.db_user, db_passwd=self.db_passwd, inputfiles=[filename], \
-										organism=self.organism, commit=self.commit, report=self.report, debug=self.debug)
-			cf2db_ins.run()
+			if os.path.isfile(filename):
+				#2010-12-14 stop passing self.organism to chromosome_fasta2db because the latter can parse tax_id out from files.
+				# and two-letter organism symbols start to be non-unique.
+				cf2db_ins = chromosome_fasta2db(drivername=self.drivername, hostname=self.hostname, dbname=self.dbname, schema=self.schema, \
+											db_user=self.db_user, db_passwd=self.db_passwd, inputfiles=[filename], \
+											organism=None, commit=self.commit, report=self.report, debug=self.debug)
+				cf2db_ins.run()
+			else:
+				sys.stderr.write("Warning: file %s doesn't exist. Skipped.\n"%(filename))
 
 if __name__ == '__main__':
 	from pymodule import ProcessOptions
